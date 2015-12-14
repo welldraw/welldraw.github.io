b2b67b0b4158b778a1f14c11c74ac2aa9515cefa
Connor <connor.stricklan@gmail.com>
2015-12-14 21:01:47 +0800

    Set up file structure
    Moved WellPaper service out of MainController file and moved csLib to
    app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,24 @@ app.value('appConst', {
     cementHeight: 100,
     cementWidth: 15,
 });
+
+app.value('csLib', {
+    isExist: function (x) {
+        return (x !== undefined && x !== null);
+    },
+    isExistPositive: function (x) {
+        return (x >= 0 && x !== undefined && x !== null);
+    },
+    useThisOrAlternate: function (x, alternate) {
+        if (x !== undefined && x !== null) {
+            return x;
+        } else {
+            return alternate;
+        }
+    }
+});
+
+//http://stackoverflow.com/questions/9308938/inline-text-editing-in-svg
+
+//@todo: get rid of clickable array
+//@todo
