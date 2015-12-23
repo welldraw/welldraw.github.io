b28308d884e20209bd933521c7b3161883e5b307
Connor <connor.stricklan@gmail.com>
2015-12-23 15:00:09 +0800

    Start to add a color chooser

---
+++ app/scripts/directives/wellColorChooser.js
@@ -0,0 +1,13 @@
+app.directive('wellColorChooser', ['$compile', 'WellPaper', function ($compile, WellPaper) {
+    'use strict';
+    return {
+        restrict: 'E',
+        template: '<img type="button" class="wd-button" ng-src="/app/content/buttons/pick_color_50x50.png" uib-tooltip="Change Color" tooltip-placement="bottom"></img>',
+        //templateUrl: '/app/scripts/directives/wellSvgTemplate.html',
+        replace: true,
+        link: function (scope, element, attr) { 
+            
+            
+        }
+    };
+}]);
