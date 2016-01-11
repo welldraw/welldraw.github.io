4faf9a7a16abe966938cb96b9fb728826c3572cf
Connor <connor.stricklan@gmail.com>
2016-01-11 21:20:32 +0800

    Use css loader in webpack for css dependencies

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,3 +4,6 @@ require("./directives/wellSvg.js");
 require("./directives/wellColorChooser.js");
 require("./directives/inputFocusFunction.js");
 require("./controllers/MainController.js");
+
+require("../../node_modules/angular-material/angular-material.css");
+require("../app.css");
